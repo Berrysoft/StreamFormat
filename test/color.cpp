c978780d6e17dbfbb8035932d78f21848c3dcd6e
王宇逸 <Strawberry_Str@hotmail.com>
2018-09-17 20:14:08 +0800

    Add color test.

--- test/color.cpp
+++ test/color.cpp
@@ -1,10 +1,15 @@
-﻿#include <sf/format.hpp>
+﻿#include <sf/sformat.hpp>
 #include <sf/ansi.hpp>
 
 using namespace sf;
+using namespace std;
 
 int main()
 {
-    print("{0}, {1}!\n", make_color_arg("Hello", yellow), make_color_arg("world", bright_cyan, blue));
+    string s = sprint("{0}, {1}!\n", make_color_arg("Hello", yellow), make_color_arg("world", bright_cyan, blue));
+    if (s == "\033[33;49mHello\033[0m, \033[96;44mworld\033[0m!\n")
+    {
+        print("Success.\n");
+    }
     return 0;
 }
